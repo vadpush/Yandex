ebc3f5ea7eb04d1d1dd030d4fdda390293989aea
vadpush <vad.push@mail.ru>
2017-10-25 14:00:58 +0300

    last task

--- array2d_practice/Project3/Project3/Source.cpp
+++ array2d_practice/Project3/Project3/Source.cpp
@@ -7,43 +7,60 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, "Russian");
 	cout << setw(4);
-	int n, m;
-	int a[100][100];
+	int n, m,k=0;
 	cin >> n >> m;
-	for (int i = 0; i <= n+1; i++) {
-		for (int j = 0; j <= m+1; j++) {
-			a[i][j] = -1;
+	int numbers = n*m,sum=1;
+	int a[100][100];
+	int x=0, y=0, x_max = m, y_max = n;
+	int x_min = 0, y_min = 0;
+	//идем по всем n*m элементам
+	while (k < numbers) {
+		//идем к правой границе
+		for (; x < x_max && k < numbers; ++x, ++k) {
+			a[y][x] = sum++;
 		}
+		//дошли до границы
+		x--;
+		y++;
+		//изменяем правую границу на 1 меньше
+		x_max--;
+
+		//идем к нижней границе
+		for (; y < y_max && k < numbers; ++y, ++k) {
+			a[y][x] = sum++;
+		}
+		//дошли до границы
+		y--;
+		x--;
+		//изменяем нижнюю границу на 1 меньше
+		y_min++;
+
+		//идем к левой границе
+		for (; x >= x_min && k < numbers; --x, ++k) {
+			a[y][x] = sum++;
+		}
+		//дошли до границы
+		x++;
+		y--;
+		//изменяем левую границу на 1 меньше
+		x_min++;
+
+		//идем к верхней границе
+		for (; y >= y_min && k < numbers; --y, ++k) {
+			a[y][x] = sum++;
+		}
+		//дошли до границы
+		y++;
+		x++;
+		//изменяем правую границу на 1 меньше
+		y_max--;
 	};
-	vector <int> b;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m; j++) {
-			a[i][j] =0;
-		}
-	};
-	int current_i = 1, current_j = 1,sum=1;
-	for (int k = 0; k < m*n; k++) {
-		a[current_i][current_j] = sum;
-		if (a[current_i][current_j+1]==0) {
-			current_j++;
-		}
-		if (a[current_i+1][current_j] == 0) {
-			current_i++;
-		}
-		if (a[current_i][current_j - 1] == 0) {
-			current_j--;
-		}
-		if (a[current_i-1][current_j] == 0 ) {
-			current_i--;
-		};
-		sum++;
-	};
-	for (int i = 1; i <=n; i++) {
-		for (int j = 1; j <= m; j++) {
-			cout << a[i][j] << setw(4);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << setw(4) << a[i][j];
 		}
 		cout << endl;
-	};
+	}
 	system("pause");
 	return 0;
 }
