75242cf7d0826918f6a344f656c7fb63ddf1d7a7
vadpush <vad.push@mail.ru>
2017-11-15 12:25:31 +0300

    2task

--- string_practice/Project1/Project1/Source.cpp
+++ string_practice/Project1/Project1/Source.cpp
@@ -3,10 +3,24 @@
 using namespace std;
 int main() {
 	setlocale(LC_ALL, "Russian");
-	char c;
+	/*char c;
 	cin >> c;
 	if (c >= '0' && c <= '9') cout << "yes";
-	else cout << "no";
+	else cout << "no";*/
+
+	char c;
+	cin >> c;
+	if (c >= 'A' && c <= 'Z') cout << c;
+	else {
+		if (c >= 'a' && c <= 'z') {
+			int al_num = c - 'a';
+			cout << (char) ('A' + al_num);
+		}
+		else {
+			cout << c;
+		}
+	}
+
 	system("pause");
 	return 0;
 }
